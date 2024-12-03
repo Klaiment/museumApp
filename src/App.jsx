b21482f8171de5a06ab6a115f774ef84d15df73d
Clement <clem@gitea.dev>
2024-12-03 18:54:03 +0100

    add popin to show more details

--- src/App.jsx
+++ src/App.jsx
@@ -6,42 +6,76 @@ import { useEffect, useState } from "react";
 import { ApiConnection } from "./data/ApiConnection.jsx";
 import { Paginator } from "./components/Paginator.jsx";
 import { SearchBar } from "./components/SearchBar.jsx";
+import { MoreDetails } from "./components/MoreDetails.jsx";
 function App() {
   const [artObjects, setArtObjects] = useState({});
   const [maxResults, setMaxResults] = useState(20);
   const [resultPage, setResultPage] = useState(1);
   const [searchQuery, setSearchQuery] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [isNeedDetails, setIsNeedDetails] = useState(false);
+  const [details, setDetails] = useState(null);
 
+
+  useEffect(() => {
+
+    if (isNeedDetails) {
+      document.documentElement.style.overflow = 'hidden';
+    }else {
+        document.documentElement.style.overflow = 'auto';
+    }
+    const handleKeyDown = (event) => {
+      if (event.key === 'Escape' && isNeedDetails) {
+        setIsNeedDetails(false);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isNeedDetails]);
   return (
     <>
-      <ApiConnection
-        setLoading={setLoading}
-        setArtObjects={setArtObjects}
-        maxResults={maxResults}
-        resultPage={resultPage}
-        searchQuery={searchQuery}
-      />
+      {isNeedDetails && (
+        <MoreDetails detail={details} setIsNeedDetails={setIsNeedDetails} />
+      )}
 
-      <>
-          <a href="/" className={"innertLink"}><h1 className="heading-1">rijksmuseum</h1></a>
-          <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
-          <div className={"container"}>
-              {loading ? (
-                  <div className={"loading"}><span className="loader"></span></div>
-              ) : (
-                  artObjects.length > 0 &&
-                  artObjects.map((artObject) => (
-                      <PaintingDisplay key={artObject.id} {...artObject} />
-                  ))
-              )}
-          </div>
-          <div className={"paginatorContainer"}>
-              <Paginator setResultPage={setResultPage} resultPage={resultPage}/>
-          </div>
-      </>
-      </>
-        );
-        }
+      <div className={`${isNeedDetails && 'blur'}`}>
+        <ApiConnection
+          setLoading={setLoading}
+          setArtObjects={setArtObjects}
+          maxResults={maxResults}
+          resultPage={resultPage}
+          searchQuery={searchQuery}
+        />
 
-        export default App;
+        <a href="/" className={"innertLink"}>
+          <h1 className="heading-1">rijksmuseum</h1>
+        </a>
+        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        <div className={"container"}>
+          {loading ? (
+            <div className={"loading"}>
+              <span className="loader"></span>
+            </div>
+          ) : (
+            artObjects.length > 0 &&
+            artObjects.map((artObject) => (
+              <PaintingDisplay
+                artObject={artObject}
+                setDetails={setDetails}
+                setIsNeedDetails={setIsNeedDetails}
+              />
+            ))
+          )}
+        </div>
+        <div className={"paginatorContainer"}>
+          <Paginator setResultPage={setResultPage} resultPage={resultPage} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
--- src/components/PaintingDisplay.jsx
+++ src/components/PaintingDisplay.jsx
@@ -1,20 +1,37 @@
 import s from "./styles/PaintingDisplay.module.css";
+import axios from "axios";
 
-export const PaintingDisplay = (key, ...artObject) => {
-  const isLandscape = key?.webImage?.width > key?.webImage?.height;
+export const PaintingDisplay = ({
+  artObject,
+  setDetails,
+  setIsNeedDetails,
+}) => {
+  const isLandscape = artObject?.webImage?.width > artObject?.webImage?.height;
+  const showDetails = async () => {
+
+    await axios.get(`https://www.rijksmuseum.nl/api/en/collection/${artObject?.objectNumber}?key=8QQ9KcWz`).then((response) => {
+      setDetails(response.data.artObject);
+    }).catch((error) => {
+      console.error(error);
+    });
+    setIsNeedDetails(true);
+  };
   return (
     <>
-      <div className={`${s.paintContainer} ${isLandscape ? s.landscape : ""}`}>
+      <div
+        className={`${s.paintContainer} ${isLandscape ? s.landscape : ""}`}
+        onClick={showDetails}
+      >
         <img
           className={s.image}
           src={
-            key?.webImage?.url ||
+            artObject?.webImage?.url ||
             "https://ih1.redbubble.net/image.4905811447.8675/flat,750x,075,f-pad,750x1000,f8f8f8.jpg"
           }
           alt=""
         />
-        <p className={s.title}>{key.title}</p>
-        <p className={s.author}>{key.principalOrFirstMaker}</p>
+        <p className={s.title}>{artObject.title}</p>
+        <p className={s.author}>{artObject.principalOrFirstMaker}</p>
       </div>
     </>
   );
