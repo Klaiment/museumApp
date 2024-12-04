201d714f4d7fc3a35f52042c4764b8928ae6620a
Clement <clem@gitea.dev>
2024-12-04 12:58:19 +0100

    prettier

--- netlify/functions/get-collection.js
+++ netlify/functions/get-collection.js
@@ -3,23 +3,21 @@ import axios from "axios";
 
 export const handler = async (event) => {
   const params = event.queryStringParameters;
-    try {
-        const response = await axios.get(
-            `https://www.rijksmuseum.nl/api/en/collection?key=${process.env.VITE_API_KEY}&ps=${params.ps}&p=${params.p}${params.q ? `&q=${params.q}` : ""}`
-        );
-        return {
-            statusCode: 200,
-            body: JSON.stringify(response.data),
-        };
-    } catch (error) {
-        console.error('Error fetching data:', error.message || error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                error: error.message || 'An unknown error occurred',
-            }),
-        };
-    }
-
-
+  try {
+    const response = await axios.get(
+      `https://www.rijksmuseum.nl/api/en/collection?key=${process.env.VITE_API_KEY}&ps=${params.ps}&p=${params.p}${params.q ? `&q=${params.q}` : ""}`,
+    );
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error.message || error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message || "An unknown error occurred",
+      }),
+    };
+  }
 };
